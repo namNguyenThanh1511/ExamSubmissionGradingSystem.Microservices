aec9ed6733c842c5398ee81e3f8eedefaa125e3a
namNguyenThanh1511 <nam151103@gmail.com>
2025-10-30 20:13:41 +0700

    feat: add test commit auth controller

--- src/Services/IAM/IAM.API/Controllers/AuthController.cs
+++ src/Services/IAM/IAM.API/Controllers/AuthController.cs
@@ -19,5 +19,17 @@ namespace IAM.API.Controllers
             var token = "...";
             return this.ToApiResponse(new { token, role = "Admin" }, "Login successful");
         }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            // ... logic
+
+            //if (!valid)
+            //    return this.ToErrorResponse("Login failed", "Invalid username or password");
+
+            var token = "...";
+            return this.ToApiResponse(new { token, role = "Admin" }, "Register successful");
+        }
     }
 }
