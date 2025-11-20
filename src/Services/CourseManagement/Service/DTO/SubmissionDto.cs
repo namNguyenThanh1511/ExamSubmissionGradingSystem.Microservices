b2973ac98c639743595238ae8fb24880ead89953
namNguyenThanh1511 <nam151103@gmail.com>
2025-11-20 21:10:24 +0700

    add student code dto

--- src/Services/CourseManagement/Service/DTO/SubmissionDto.cs
+++ src/Services/CourseManagement/Service/DTO/SubmissionDto.cs
@@ -1,11 +1,10 @@
-using System;
-
 namespace Service.DTO
 {
     public class SubmissionDto
     {
         public long Id { get; set; }
         public long? ExamId { get; set; }
+        public string StudentCode { get; set; } = string.Empty;
         public Guid? SubmissionId { get; set; }
         public double? TotalScore { get; set; }
         public long? ExaminerId { get; set; }
